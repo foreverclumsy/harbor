f695c9af66e87fda569bd53609d04222d01654d8
Meghan Richards <megatron@Meghans-MacBook-Pro.local>
2026-06-14 22:56:05 -0500

    Add Cloudflare Pages proxy

---
+++ functions/api-proxy/[[path]].js
@@ -0,0 +1,31 @@
+export async function onRequest(context) {
+  const path = context.params.path;
+
+  if (!path || path.length < 2) {
+    return new Response("Invalid proxy request", { status: 400 });
+  }
+
+  const host = path[0];
+  const targetPath = "/" + path.slice(1).join("/");
+
+  const url = new URL(context.request.url);
+  const target = `https://${host}${targetPath}${url.search}`;
+
+  const headers = new Headers(context.request.headers);
+  headers.delete("host");
+
+  const response = await fetch(target, {
+    method: context.request.method,
+    headers,
+    body:
+      context.request.method === "GET" ||
+      context.request.method === "HEAD"
+        ? undefined
+        : await context.request.arrayBuffer(),
+  });
+
+  return new Response(response.body, {
+    status: response.status,
+    headers: response.headers,
+  });
+}
